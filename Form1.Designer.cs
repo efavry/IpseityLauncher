3804180406785ec21e33f09d7d243c38a20f6f5e
Favry Erwan <erwan.favry@utbm.fr>
2015-05-11 00:25:51 +0200

    First Commit of stable release.

---
+++ Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace IpseityLauncher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button_LaunchIpseity = new System.Windows.Forms.Button();
+            this.textBox_RootFolder = new System.Windows.Forms.TextBox();
+            this.textBox_PrologFolder = new System.Windows.Forms.TextBox();
+            this.label_IpseityRootFolder = new System.Windows.Forms.Label();
+            this.label_PrologFolder = new System.Windows.Forms.Label();
+            this.textBox_Status = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button_LaunchIpseity
+            // 
+            this.button_LaunchIpseity.Location = new System.Drawing.Point(387, 65);
+            this.button_LaunchIpseity.Name = "button_LaunchIpseity";
+            this.button_LaunchIpseity.Size = new System.Drawing.Size(103, 23);
+            this.button_LaunchIpseity.TabIndex = 0;
+            this.button_LaunchIpseity.Text = "Launch Ipseity";
+            this.button_LaunchIpseity.UseVisualStyleBackColor = true;
+            this.button_LaunchIpseity.Click += new System.EventHandler(this.button_LaunchIpseity_Click);
+            // 
+            // textBox_RootFolder
+            // 
+            this.textBox_RootFolder.Location = new System.Drawing.Point(114, 10);
+            this.textBox_RootFolder.Name = "textBox_RootFolder";
+            this.textBox_RootFolder.Size = new System.Drawing.Size(376, 20);
+            this.textBox_RootFolder.TabIndex = 1;
+            this.textBox_RootFolder.Text = "C:\\Program Files (x86)\\ipseity_1-2-2_kernel_bin_win";
+            // 
+            // textBox_PrologFolder
+            // 
+            this.textBox_PrologFolder.Location = new System.Drawing.Point(114, 39);
+            this.textBox_PrologFolder.Name = "textBox_PrologFolder";
+            this.textBox_PrologFolder.Size = new System.Drawing.Size(376, 20);
+            this.textBox_PrologFolder.TabIndex = 2;
+            this.textBox_PrologFolder.Text = "C:\\Program Files (x86)\\pl";
+            // 
+            // label_IpseityRootFolder
+            // 
+            this.label_IpseityRootFolder.AutoSize = true;
+            this.label_IpseityRootFolder.Location = new System.Drawing.Point(12, 10);
+            this.label_IpseityRootFolder.Name = "label_IpseityRootFolder";
+            this.label_IpseityRootFolder.Size = new System.Drawing.Size(101, 13);
+            this.label_IpseityRootFolder.TabIndex = 3;
+            this.label_IpseityRootFolder.Text = "Ipseity Root Folder :";
+            // 
+            // label_PrologFolder
+            // 
+            this.label_PrologFolder.AutoSize = true;
+            this.label_PrologFolder.Location = new System.Drawing.Point(12, 39);
+            this.label_PrologFolder.Name = "label_PrologFolder";
+            this.label_PrologFolder.Size = new System.Drawing.Size(75, 13);
+            this.label_PrologFolder.TabIndex = 4;
+            this.label_PrologFolder.Text = "Prolog Folder :";
+            // 
+            // textBox_Status
+            // 
+            this.textBox_Status.Location = new System.Drawing.Point(114, 68);
+            this.textBox_Status.Name = "textBox_Status";
+            this.textBox_Status.ReadOnly = true;
+            this.textBox_Status.Size = new System.Drawing.Size(267, 20);
+            this.textBox_Status.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Status :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(502, 100);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_Status);
+            this.Controls.Add(this.label_PrologFolder);
+            this.Controls.Add(this.label_IpseityRootFolder);
+            this.Controls.Add(this.textBox_PrologFolder);
+            this.Controls.Add(this.textBox_RootFolder);
+            this.Controls.Add(this.button_LaunchIpseity);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "IpseityLauncher";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_LaunchIpseity;
+        private System.Windows.Forms.TextBox textBox_RootFolder;
+        private System.Windows.Forms.TextBox textBox_PrologFolder;
+        private System.Windows.Forms.Label label_IpseityRootFolder;
+        private System.Windows.Forms.Label label_PrologFolder;
+        private System.Windows.Forms.TextBox textBox_Status;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,176 @@
+﻿
+/*This program use a tango icon:
+ * "By The people from the Tango! project (The Tango! Desktop Project) [Public domain or Public domain], via Wikimedia Commons"
+ */
+
+using System;
+using System.IO;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Collections.Specialized;
+
+namespace IpseityLauncher
+{
+    public partial class Form1 : Form
+    {
+        private String stringIpseityRootFolder;
+        private String stringPrologFolder;
+        private Boolean boolIpseityFoundOnStart;
+        private Boolean boolPrologFoundOnStart;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// The goal of this method is to find automatically the 
+        /// pathe needed by ipseity, it we don't find it, it will fallback
+        /// to the classical way : asking the user.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                // Get the current directory in the case of the launcher present in the ipseity folder and
+                //test the validity of the asumption.
+                String stringCurrentDirectory = Directory.GetCurrentDirectory();
+                if (File.Exists(stringCurrentDirectory + @"\app\IpseityProject\binary\1.2.2\win\Ipseity.exe"))
+                {
+                    //Hiding all that is unnecessary and put the correct path
+                    textBox_RootFolder.Text = stringCurrentDirectory;
+                    label_IpseityRootFolder.Visible = false;
+                    textBox_RootFolder.Visible = false;
+                    textBox_Status.Text = "Ipseity found. ";
+                    boolIpseityFoundOnStart=true;
+                }
+
+                //Same here but for prolog in the program files folder
+                String string32BitProgramFilesFolder = this.Get32bitProgramFiles();
+                if(File.Exists(string32BitProgramFilesFolder + @"\pl\bin\plcon.exe"))
+                {
+                    //Hiding all that is unnecessary and put the correct path
+                    textBox_PrologFolder.Text = string32BitProgramFilesFolder + @"\pl";
+                    label_PrologFolder.Visible = false;
+                    textBox_PrologFolder.Visible = false;
+                    textBox_Status.Text = textBox_Status.Text + "Prolog found. ";
+                    boolPrologFoundOnStart =true;
+                }
+                if (boolPrologFoundOnStart && boolIpseityFoundOnStart)
+                {
+                    textBox_Status.Text = textBox_Status.Text + "Automatic Launch. ";
+                    this.launch();
+                }
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                MessageBox.Show("The launcher does not have enough right to access the current directory" + ex.ToString());
+            }
+        }
+
+        /// <summary>
+        /// This method will get the path given by the user
+        /// It will do some test to check the validity of the paths
+        /// Then we make the adequate path and we run ipseity
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void launch() 
+        {
+            //By security we desactivate te textbox while working
+            textBox_RootFolder.Enabled = false;
+            textBox_PrologFolder.Enabled = false;
+
+            //We get the text of the textbox
+            stringIpseityRootFolder = textBox_RootFolder.Text;
+            stringPrologFolder = textBox_PrologFolder.Text;
+
+            if (//Test if the path are correct by checking on of the essential file that must be contained
+                File.Exists(stringIpseityRootFolder + @"\app\IpseityProject\binary\1.2.2\win\Ipseity.exe") &&
+                File.Exists(stringPrologFolder + @"\bin\plcon.exe")
+                )
+            {
+                //Concatenate all the path correctly without overwriting the content of the original
+                Environment.SetEnvironmentVariable(
+                                                    "PATH", Environment.GetEnvironmentVariable("PATH") +
+                    /*The Lib to know :*/ stringIpseityRootFolder + @"\lib\win\Qt_Libraries" +
+                                                    ";" +
+                    /*prolog path :*/     stringPrologFolder + @"\bin" +
+                                                    ";");
+                //Launching ipseity
+                //Process.Start(stringIpseityRootFolder + @"\app\IpseityProject\binary\1.2.2\win\Ipseity.exe");
+
+                ProcessStartInfo processIpseity = new ProcessStartInfo(stringIpseityRootFolder + @"\app\IpseityProject\binary\1.2.2\win\Ipseity.exe");
+
+                processIpseity.UseShellExecute = false; //mandatory if not false then process.start throw exception
+
+
+                //working on dictionarry
+                processIpseity.EnvironmentVariables.Remove("PATH"); //remove the previous path
+
+                //get the local path, reinject it into the path variable and add the other path needed by ipseity
+                processIpseity.EnvironmentVariables.Add(
+                                                        "PATH",
+                                                        Environment.GetEnvironmentVariable("PATH") +
+                    /*The Lib to know :*/ stringIpseityRootFolder + @"\lib\win\Qt_Libraries" +
+                                                        ";" +
+                    /*prolog path :*/     stringPrologFolder + @"\bin" +
+                                                        ";");
+
+                //so that ipseity know where he is working
+                processIpseity.WorkingDirectory = stringIpseityRootFolder + @"\app\IpseityProject\binary\1.2.2\win";
+                
+                //Inform the user :
+                textBox_Status.Text = textBox_Status.Text + "Launching. ";
+
+                //Finally launching
+                Process.Start(processIpseity);
+
+            }
+            else
+            {
+                //Signal that the path are inccorect
+                MessageBox.Show("The Ipseity folder or the prolog folder seems to be wrong \n");
+            }
+            //Get back to normal
+            textBox_RootFolder.Enabled = true;
+            textBox_PrologFolder.Enabled = true;
+        }
+
+        /// <summary>
+        /// Try to launch ipseity on click of the user
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_LaunchIpseity_Click(object sender, EventArgs e)
+        {
+            this.launch();
+        }
+
+
+        /// <summary>
+        /// The goal of this function is to get the path of the 
+        /// 32bit Program Files Folder, whereas you are or not 
+        /// in a 64bit os. Not so much usefull....
+        /// </summary>
+        /// <returns>A string refering to the places of the 32 Bit Program Files</returns>
+        private String Get32bitProgramFiles()
+        {
+            if (Environment.Is64BitOperatingSystem)
+            {
+                return Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86);
+            }
+            return System.Environment.GetFolderPath(System.Environment.SpecialFolder.ProgramFiles);
+        }
+
+    }
+}
